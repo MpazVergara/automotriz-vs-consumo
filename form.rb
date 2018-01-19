aaa04c66ef66d0c1bfc6cab951f5a809d01848c0
Maria Paz <maria.paz.vergara@continuum.cl>
2018-01-19 14:07:37 -0300

    Simplify form

---
+++ form.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'bci'
+require 'date'
+
+BCI = Bci::Client.new({ key: ENV['BCI_API_KEY'] })
+
+get '/' do
+  "Hola mundo"
+end
+
+get '/form' do
+  form_tag do
+    label_tag(:my_name, "My name is:")
+    text_field_tag(:my_name)
+    submit_tag("Process")
+  end
+end
+
+get '/simulacion' do
+  erb :simulacion_consumo
+end
+
+post '/simulacion_BCI' do
+  params["Amnt"]=Integer(params[:valcuota])*Integer(params[:cantidadCuotas])
+  params["rut"],params["dv"],params["renta"]=["77777777","7","200000"]
+  params.to_s
+  cons = BCI.consumo.simulate("1",params)
+  print params["ACAE"]
+  print cons
+  cons.to_s
+  erb :simulacion_consumo_bci, :locals => {
+    :acae => params["ACAE"], :amnt => params["Amnt"], :actc => params["Actc"],
+    :cmnt => cons["montoCredito"], :ccae => cons["montoCae"], :cctc => cons["montoCtc"]}
+end
