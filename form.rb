6f71f1c2759d311f316a8f716c46a24f13bd8629
Maria Paz <maria.paz.vergara@continuum.cl>
2018-01-23 17:56:22 -0300

    Validate separador de miles

--- form.rb
+++ form.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 require 'bci'
 require 'date'
 
+def separador_miles(numero)
+  numero = String(numero).gsub(/\./,"")
+  numero = numero.reverse!.gsub(/(?=\d*\.?)(\d{3})/){$1+'.'}
+  numero = numero.reverse!.gsub(/^[\.]/,"")
+  return numero
+end
+
 BCI = Bci::Client.new({ key: ENV['BCI_API_KEY'] })
 
 get '/' do
@@ -13,14 +20,11 @@ get '/simulacion' do
 end
 
 post '/simulacion_BCI' do
-  params["Amnt"]=Integer(params[:valcuota])*Integer(params[:cantidadCuotas])
-  params["rut"],params["dv"],params["renta"]=["77777777","7","200000"]
-  params.to_s
+  params["Amnt"]=Integer(params[:valcuota].sub(/\./,''))*Integer(params[:cantidadCuotas].sub(/\./,''))
+  params["rut"],params["dv"],params["renta"],params["montoCredito"]=["77777777","7","200000",params["montoCredito"].sub(/\./,'')]
   cons = BCI.consumo.simulate("1",params)
-  print params["ACAE"]
-  print cons
-  cons.to_s
-  erb :simulacion_consumo_bci, :locals => {
-    :acae => params["ACAE"], :amnt => params["Amnt"], :actc => params["Actc"],
-    :cmnt => cons["montoCredito"], :ccae => cons["montoCae"], :cctc => cons["montoCtc"]}
+  comparacion=Integer(params["Amnt"]) - Integer(cons["montoCredito"])
+  erb :simulacion_consumo_bci, :locals => { :comp => comparacion,
+    :amnt => separador_miles(params["Amnt"]), :acae => String(params["ACAE"]).sub(/\./,','), :actc => params["Actc"],
+    :cmnt => separador_miles(cons["montoCredito"]), :ccae => String(cons["montoCae"]).gsub(/\./,','), :cctc => separador_miles(cons["montoCtc"])}
 end
